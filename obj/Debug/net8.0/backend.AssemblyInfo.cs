58c1407dc32a241a38e3c97f0d291e2479a1aafe
Jonathan <jongroberg@gmail.com>
2024-06-26 14:49:50 -0400

    tweaks

--- obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80669a5373e6fc07e5d0147ce7c40766d8f7421d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5e1b5d431867e8496573381c2efc0b61603a652")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
